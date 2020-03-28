e903e2d168db961097139775e1c36bcdfaf60038
iMasanari <14dd023@gmail.com>
2020-03-28 22:14:25 +0900

    feat: 印刷ボタンを実装

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,5 +1,5 @@
 import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer'
-import React from 'react'
+import React, { useRef } from 'react'
 import PDF from '~/pdf/PDF'
 
 require('./Preview.css')
@@ -12,16 +12,33 @@ interface Props {
 }
 
 export default ({ size, orientation, list, className }: Props) => {
+  const previewRef = useRef<HTMLDivElement>(null)
   const pdf = <PDF size={size} orientation={orientation} list={list} />
 
+  const print = () => {
+    const target = previewRef.current?.querySelector('iframe')?.contentWindow
+
+    if (!target) return
+
+    try {
+      target.print()
+    } catch {
+      // Firefoxでエラーになる（cross-origin関連）
+      alert('印刷画面を開くことができませんでした。\nプレビュー内に印刷ボタンがある場合は、そこから印刷できます。または、ダウンロードして印刷してください。')
+    }
+  }
+
   return (
-    <div className={['Preview', className].join(' ')}>
+    <div className={['Preview', className].join(' ')} ref={previewRef}>
       <PDFViewer className="Preview-pdf">
         {pdf}
       </PDFViewer>
       <div className="Preview-footer">
-        <PDFDownloadLink document={pdf} className="Preview-download">
-          {'印刷データをダウンロード'}
+        <span className="Preview-button" role="button" onClick={print}>
+          {'印刷'}
+        </span>
+        <PDFDownloadLink document={pdf} className="Preview-button">
+          {'ダウンロード'}
         </PDFDownloadLink>
       </div>
     </div>
