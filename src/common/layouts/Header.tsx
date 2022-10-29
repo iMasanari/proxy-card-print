b5208972a6a08720a890602a2757a4dca698f3c0
iMasanari <14dd023@gmail.com>
2022-10-29 09:33:45 +0900

    Tweetアイコンを追加

--- src/common/layouts/Header.tsx
+++ src/common/layouts/Header.tsx
@@ -1,12 +1,20 @@
-import { AppBar, Toolbar, Typography } from '@mui/material'
-import React from 'react'
+import { css } from '@emotion/react'
+import { Twitter } from '@mui/icons-material'
+import { AppBar, Button, Toolbar, Typography } from '@mui/material'
+
+const titleStyle = css`
+  margin-right: auto;
+`
 
 const Header = () =>
   <AppBar position="static">
     <Toolbar>
-      <Typography component="h1" variant="h6">
+      <Typography component="h1" variant="h6" css={titleStyle}>
         プロキシカード印刷
       </Typography>
+      <Button color="inherit" startIcon={<Twitter />} href="https://twitter.com/intent/tweet?text=プロキシカード印刷｜PCスマホで簡単作成、コンビニ印刷！%20%23プロキシ印刷%0A&url=https%3A%2F%2Fimasanari.github.io%2Fproxy-card-print%2F">
+        Tweet
+      </Button>
     </Toolbar>
   </AppBar>
 
