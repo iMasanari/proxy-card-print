7ed59b4cb6677a88585a84db0333732a95fe03e8
iMasanari <14dd023@gmail.com>
2023-02-20 22:12:47 +0900

    もしかして機能を改良して再実装

---
+++ src/app/features/maybe/Maybe.tsx
@@ -0,0 +1,81 @@
+import { List, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material'
+import { useEffect, useMemo, useState } from 'react'
+import { SettingsCard } from '../cards/cardsReducer'
+import { getItem } from './items'
+
+interface Props {
+  cards: SettingsCard[]
+}
+
+interface CardData {
+  name: string
+  url: string
+}
+
+const Item = ({ card }: { card: Blob }) => {
+  const [cardData, setCardNames] = useState<CardData[]>([])
+
+  useEffect(() => {
+    (async () => {
+      const src = URL.createObjectURL(card)
+      const image = new Image()
+
+      await new Promise(resolve => {
+        image.onload = resolve
+        image.src = src
+      })
+
+      const canvas = document.createElement('canvas')
+      const ctx = canvas.getContext('2d')!
+
+      canvas.width = image.width
+      canvas.height = image.height
+
+      ctx.drawImage(image, 0, 0)
+
+      const pixcelData = ctx.getImageData(0, 0, image.width, image.height)
+
+      // TODO: エラーキャッチ
+      const cardNames = await getItem({ image: pixcelData }).catch((): string[] => [])
+
+      setCardNames(cardNames.map(name => {
+        const url = new URL('https://www.amazon.co.jp/s')
+        url.searchParams.set('k', name)
+
+        return { name, url: url.toString() }
+      }))
+
+      URL.revokeObjectURL(src)
+    })()
+  }, [card])
+
+  return (
+    <>
+      {cardData.map(v =>
+        <ListItem key={v.name} disablePadding>
+          <ListItemButton href={v.url} target="_blank" rel="noopener">
+            <ListItemText primary={v.name} secondary="Amazonで価格を見る" />
+          </ListItemButton>
+        </ListItem>
+      )}
+    </>
+  )
+}
+
+export default ({ cards }: Props) => {
+  return (
+    <div>
+      <Typography variant="body2" fontWeight="bolder" sx={{ textAlign: 'center' }}>
+        もしかして(β)
+      </Typography>
+      <List>
+        {cards.map(card =>
+          <Item key={card.id} card={card.file} />
+        )}
+      </List>
+      <Typography variant="body2" fontSize="0.7em" p={1} gutterBottom>
+        ※追加した画像をもとに、上記リンクを生成しています。カード識別はこの端末上で行われるので、追加した画像がサーバー等に送信されることはありません。なお、この機能は一部のカードゲームのカードのみに対応しています。（現在は、ONEPIECEカードとポケモンのスタンダードレギュレーションのカードのみ）
+      </Typography>
+    </div>
+  )
+}
