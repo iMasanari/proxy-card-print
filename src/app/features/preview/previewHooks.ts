85a6581a996290e65445ac0e98548e7d8cc6f3fc
iMasanari <14dd023@gmail.com>
2024-06-22 08:15:06 +0900

    fix: カードサイズに大きな値を入れてもクラッシュしないように修正

--- src/app/features/preview/previewHooks.ts
+++ src/app/features/preview/previewHooks.ts
@@ -6,12 +6,19 @@ import { PageSize, pageSizes } from '~/domains/settings'
 const toInt = (str: string, defaultValue = NaN) =>
   /^([1-9][0-9]*|0)$/.test(str) ? +str : defaultValue
 
+const pageMargin = 7
+
 export const usePreviewData = (settingsForm: SettingsState, cardsForm: CardsState) => {
   const settings = useMemo(() => {
     const pageSize = pageSizes[settingsForm.pageSize as PageSize] ? settingsForm.pageSize as PageSize : 'A4'
     const [pageWidth, pageHeight] = pageSizes[pageSize]
-    const cardWidth = Math.min(Math.max(1, toInt(settingsForm.cardWidth, 0)), pageWidth)
-    const cardHeight = Math.min(Math.max(1, toInt(settingsForm.cardHeight, 0)), pageHeight)
+
+    // FIXME: ./parts/Page.tsx、./Preview.tsxと重複している
+    const maxCardWidth = pageWidth - (pageMargin + 1) * 2
+    const maxCardHeight = pageHeight - (pageMargin + 1) * 2
+
+    const cardWidth = Math.min(Math.max(1, toInt(settingsForm.cardWidth, 0)), maxCardWidth)
+    const cardHeight = Math.min(Math.max(1, toInt(settingsForm.cardHeight, 0)), maxCardHeight)
 
     const cards = cardsForm.map(card => ({
       ...card,
