45bd22ee50eb59983f2a4aa9fe4846ff78caf147
iMasanari <14dd023@gmail.com>
2024-11-16 13:24:34 +0900

    数値入力エラー時に文字色が赤になるよう変更

--- src/app/common/atoms/NumberField.tsx
+++ src/app/common/atoms/NumberField.tsx
@@ -1,31 +1,22 @@
-import { css } from '@emotion/react'
 import { TextField, TextFieldProps } from '@mui/material'
-import React from 'react'
 
 type Props = TextFieldProps & {
   min?: number
   max?: number
-  spinButton?: boolean
 }
 
-const noSpinButtonStyle = css`
-  & input {
-    -moz-appearance: textfield;
-  }
-  & input::-webkit-inner-spin-button,
-  & input::-webkit-outer-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  }
-`
+const NumberField = ({ min, max, required, inputProps, ...props }: Props) => {
+  const isError = !/^(0|[1-9]\d*)$/.test(props.value?.toString().trim() || '0') || (required && props.value === '')
 
-const NumberField = ({ min, max, spinButton, inputProps, ...props }: Props) => {
   return (
     <TextField
-      type="number"
-      css={spinButton === false && noSpinButtonStyle}
+      error={isError}
       inputProps={{
-        sx: { textAlign: 'right' },
+        sx: {
+          textAlign: 'right',
+          color: isError ? 'error.main' : undefined,
+          ...inputProps?.sx,
+        },
         inputMode: 'numeric',
         pattern: '[0-9]*',
         min,
